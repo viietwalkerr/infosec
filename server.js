ec445fbab3d477e2f8e291cf33e59a5f3d1622ef
NATHANPC\NateBB <nathan.thai@blackbook.ai>
2022-06-21 15:15:30 +1000

    changing bcrypt require to const

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 app.disable("x-powered-by");
 var fs = require("fs");
 var path = require("path");
-var bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 
 app.use(function (req, res, next) {
   res.set({
